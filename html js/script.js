e4009e465da282f75757932cb144e58b2873a23b
welisonbonetti <welisonbonetti17@gmail.com>
2025-09-04 21:49:53 -0300

    javascript

---
+++ html js/script.js
@@ -0,0 +1,17 @@
+// alert("ola mundo")
+// alert("oi")
+// alert("tudo bem?")
+// alert("você vem sempre aqui?")
+
+// let idade 
+// idade = 18
+// alert(idade)
+// idade = "DEZOITO"
+// alert(idade)
+const amor = "meu primeiro amor"
+alert(amor)
+let amor2 ="meu segundo amor"
+alert(amor2)
+
+
+
