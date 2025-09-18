08616f58edfad537c8e10e6bab0bb310bdf47ff2
welisonbonetti <welisonbonetti17@gmail.com>
2025-09-17 21:28:04 -0300

    atualizaçao

---
+++ ex53.js
@@ -0,0 +1,18 @@
+let placa
+placa = Number(prompt("Qual o ultimo digito da sua placa: "))
+if( placa == 0 || placa == 1){
+    alert("nao pode rodar na segunda-feira")
+}
+ else if( placa == 2 || placa == 3){
+    alert("nao pode rodar na terça-feira")
+}
+ else if( placa == 4 || placa == 5){
+    alert("nao pode rodar na quarta-feira")
+}
+else if( placa == 6 || placa == 7){
+    alert("nao pode rodar na quinta-feira")
+}
+else if( placa == 8 || placa == 9){
+    alert("nao pode rodar na sexta-feira")
+}
+
